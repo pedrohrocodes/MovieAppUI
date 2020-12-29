7153d2116376ecb14fd305b03ddcd33b6f07fb0e
devpedrohro <devpedrohro@gmail.com>
2020-12-29 17:47:56 -0300

    Done

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -19,7 +19,7 @@ class HomeScreen extends StatelessWidget {
       elevation: 0,
       leading: IconButton(
         padding: EdgeInsets.only(left: kDefaultPadding),
-        icon: SvgPicture.asset("assets/icons/manu.svg"),
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
         onPressed: () {},
       ),
       actions: [
