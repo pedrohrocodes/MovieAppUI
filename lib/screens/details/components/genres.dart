c5443f295e9645343a26729b37f2d829bf77a2cf
devpedrohro <devpedrohro@gmail.com>
2020-12-29 16:55:15 -0300

    Details Genres

---
+++ lib/screens/details/components/genres.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app_ui/constants.dart';
+import 'package:movie_app_ui/models/movie.dart';
+
+class Genres extends StatelessWidget {
+  final Movie movie;
+
+  const Genres({Key key, this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+      child: SizedBox(
+        height: 36,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movie.genra.length,
+            itemBuilder: (context, index) =>
+                GenreCard(genre: movie.genra[index])),
+      ),
+    );
+  }
+}
